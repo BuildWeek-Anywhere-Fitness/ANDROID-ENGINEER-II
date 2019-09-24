b5c398e1fe39ae8cad79fa85b9795fc6c9d8767a
tjclawson <tjamesclawson@gmail.com>
2019-09-23 20:46:16 -0400

    fixing merge conflicts

--- app/src/main/java/com/example/anywherefitness/ui/Instructor/InstructorClassesViewModel.kt
+++ app/src/main/java/com/example/anywherefitness/ui/Instructor/InstructorClassesViewModel.kt
@@ -15,6 +15,8 @@ class InstructorClassesViewModel : ViewModel() {
     private val _text = MutableLiveData<String>().apply {
         value = "This is home Fragment"
     }
+
+    
     val text: LiveData<String> = _text
 
     fun setupRecycler(context: Context?,
